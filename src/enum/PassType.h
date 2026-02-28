3aad14d427221bddaeb28fc23fedd202e23b2ab4
facobackup <gustavomicaelbarbosa@gmail.com>
2026-02-28 19:27:22 -0300

    Refactoring DescriptorInstance and DescriptorService

---
+++ src/enum/PassType.h
@@ -0,0 +1,15 @@
+#ifndef PASS_TYPE_H
+#define PASS_TYPE_H
+
+namespace Metal {
+    enum PassType {
+        GBUFFER,
+        COMPUTE,
+        POST_PROCESSING,
+        SELECTED_DOT,
+        GRID,
+        ICONS
+    };
+}
+
+#endif //PASS_TYPE_H
